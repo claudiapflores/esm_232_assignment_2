4b7405d4a23ce472182ff0891006f07e9e690fe6
claudiapflores <cpflores@bren.ucsb.edu>
2021-04-09 11:22:51 -0700

    Fixed the almond_model.R function and added more detail. Aggregrated climate data frame values and ran them through the model and checked if answers were correct.

--- R/almond_model.R
+++ R/almond_model.R
@@ -2,14 +2,15 @@
 #' 
 #' This model calculates the statistical yield 
 #' of almonds given certain inputs and parameters.
-#' @param Tn minimum temperature (°C)
-#' @param P precipitation (mm)
+#' @param Tn_2 minimum temperature for second month of year (°C)
+#' @param P_1 precipitation for first month of year (mm)
+#' @return Yield anomaly per ton acre
 
 
 almond_model = function(Tn, P) {
   
   # calculate yield
-  result = -0.015 * Tn - 0.0046 * Tn^2 - 0.07 * P + 0.0043 * P^2 + 0.28
+  result = (-0.015 * Tn_2) - (0.0046 * (Tn_2)^2) - (0.07 * P_1) + (0.0043 * (P_1)^2) + (0.28)
   
   return(result)
   
